b24fccead62c2dd4eae3569a5de1339b423597cb
Diego Joaquin Colina <diego_joa@hotmail.com>
2025-04-18 12:52:57 -0300

    Ejercicio 2 del trabajo practico 1
    Ejercicio 2 del trabajo practico 1

---
+++ Ejercico02TP01.py
@@ -0,0 +1,4 @@
+# 2. Escriba un programa que solicite el nombre del usuario y luego muestre el mensaje de salida
+
+nombre = input("Ingrese su nombre: ")
+print(f"Hola {nombre} bienvenido al curso de Python!") 
