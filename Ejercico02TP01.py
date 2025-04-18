1db3c16e453ae9ba1ef1c07b19294cb23955be24
Diego Joaquin Colina <diego.joaquin.colina@gmail.com>
2025-04-18 12:54:24 -0300

    Update Ejercico02TP01.py
    Se modifico los comentarios en el codigo

--- Ejercico02TP01.py
+++ Ejercico02TP01.py
@@ -1,4 +1,5 @@
 # 2. Escriba un programa que solicite el nombre del usuario y luego muestre el mensaje de salida
+# “Hola nombre”, donde nombre es el nombre que ingresó el usuario.
 
 nombre = input("Ingrese su nombre: ")
 print(f"Hola {nombre} bienvenido al curso de Python!") 
